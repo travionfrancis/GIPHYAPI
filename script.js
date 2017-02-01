4b762b8a3a45df7b007481a85e591bde1317b59e
gunshow <travi1500@gmail.com>
2017-02-01 23:33:25 +0000

    Giphy activity

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
     $("#giphy").submit(function(event) {
         event.preventDefault();
+        var query = $("#query").val();
+        console.log(query);
+        $.ajax({
+            url: "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC"
+        }).done(function(response) {
+            console.log(response.data[1].embed_url);
+
+            var url = response.data[1].embed_url;
+
+            var image = $('<img>');
+            image.attr('src', url);
+            $("#results").append(image);
+
+            image[0].src = response.data[0].images.fixed_width_downsampled.url;
+
+            $("#results").append(image);
+        });
     })
 });
